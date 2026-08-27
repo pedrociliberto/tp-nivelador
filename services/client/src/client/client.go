abd079f6aafef3a698a5888980098fcfcf7674c3
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-08-27 18:09:22 -0300

    Implementacion inicial Ejercicio 5 (client y server sin separacion de responsabilidades)

--- services/client/src/client/client.go
+++ services/client/src/client/client.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"net"
 	"time"
+	"io"
 	"os"
+	"strconv"
 
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/logger"
 	"github.com/7574-sistemas-distribuidos/tp-nivelador/src/safe_socket"
@@ -12,10 +14,6 @@ import (
 
 const CONNECTION_ATTEMPTS_MAX = 3
 const CONNECTION_ATTEMPS_DELAY_MS = 200
-
-const ECHO_CLIENT_BUFFER_SIZE = 512
-const ECHO_CLIENT_MESSAGE_AMOUNT = 3
-const ECHO_CLIENT_MESSAGE_DELAY_MS = 1000
 
 type ClientConfig struct {
 	ServerHost string
@@ -61,34 +59,7 @@ func connectToServer(host, port string) (net.Conn, error) {
 }
 
 func (client *Client) Run() error {
-	const mainAction = "test-echo-server"
 	defer client.conn.Close()
-
-	for messageId := range ECHO_CLIENT_MESSAGE_AMOUNT {
-		messageArgs := []any{"agency-id", client.config.AgencyId, "message-id", messageId}
-		logger.Info(mainAction, logger.InProgress, messageArgs...)
-
-		clientMessage := client.config.AgencyId
-
-		if err := safe_socket.SendAll(client.conn, []byte(clientMessage)); err != nil {
-			logger.Error("send-message", logger.Fail, messageArgs...)
-			return err
-		}
-
-		responseBuffer, err := safe_socket.RecvAll(client.conn, ECHO_CLIENT_BUFFER_SIZE)
-		if err != nil {
-			logger.Error("recv-response", logger.Fail, messageArgs...)
-			return err
-		}
-
-		if string(responseBuffer) != clientMessage {
-			logger.Error("check-response", logger.Fail, messageArgs...)
-			return err
-		}
-
-		time.Sleep(ECHO_CLIENT_MESSAGE_DELAY_MS * time.Millisecond)
-	}
-	logger.Info(mainAction, logger.Success, "agency-id", client.config.AgencyId)
 
 	if err := sendAndReceiveBets(client.conn, client.config.AgencyId); err != nil {
 		logger.Error("send-and-receive-bets", logger.Fail, "agency-id", client.config.AgencyId, "err", err)
@@ -119,26 +90,29 @@ func sendAndReceiveBets(conn net.Conn, agencyId string) error {
 	}
 	defer outputFile.Close()
 
+	agencyNum, err := strconv.Atoi(agencyId)
+	if err != nil {
+		logger.Error("parse-agency-id", logger.Fail, "agency-id", agencyId, "err", err)
+		return err
+	}
+	header := intToBytes(uint32(agencyNum))
+	if err := safe_socket.SendAll(conn, header); err != nil {
+		return err
+	}
+	
 	scanner := bufio.NewScanner(inputFile)
 	for scanner.Scan() {
 		line := scanner.Text()
+		lineBytes := []byte(line)
+		length := len(lineBytes)
+		header := intToBytes(uint32(length))
 
-		if err := safe_socket.SendAll(conn, []byte(line)); err != nil {
-			logger.Error("send-bet", logger.Fail, "agency-id", agencyId, "line", line, "err", err)
+		if err := safe_socket.SendAll(conn, header); err != nil {
+			logger.Error("send-bet-header", logger.Fail, "agency-id", agencyId, "err", err)
 			return err
 		}
-
-		responseBuffer, err := safe_socket.RecvAll(conn, ECHO_CLIENT_BUFFER_SIZE)
-		if err != nil {
-			logger.Error("recv-response", logger.Fail, "agency-id", agencyId, "err", err)
-			return err
-		}
-
-		if _, err := outputFile.Write(responseBuffer); err != nil {
-			logger.Error("write-output-file", logger.Fail, "agency-id", agencyId, "err", err)
-			return err
-		}
-		if _, err := outputFile.Write([]byte("\n")); err != nil {
+		if err := safe_socket.SendAll(conn, lineBytes); err != nil {
+			logger.Error("send-bet-payload", logger.Fail, "agency-id", agencyId, "err", err)
 			return err
 		}
 	}
@@ -148,6 +122,56 @@ func sendAndReceiveBets(conn net.Conn, agencyId string) error {
 		return err
 	}
 
+	endHeader := make([]byte, 4)
+	if err := safe_socket.SendAll(conn, endHeader); err != nil {
+		return err
+	}
+
+	for {
+		header, err := safe_socket.RecvAll(conn, 4)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			logger.Error("recv-winner-header", logger.Fail, "agency-id", agencyId, "err", err)
+			return err
+		}
+		msgLen := bytesToInt(header)
+		if msgLen == 0 {
+			break
+		}
+
+		winnerBuffer, err := safe_socket.RecvAll(conn, int(msgLen))
+		if err != nil {
+			logger.Error("recv-winner-payload", logger.Fail, "agency-id", agencyId, "err", err)
+			return err
+		}
+
+		if _, err := outputFile.Write(winnerBuffer); err != nil {
+			logger.Error("write-output-file", logger.Fail, "agency-id", agencyId, "err", err)
+			return err
+		}
+		if _, err := outputFile.Write([]byte("\n")); err != nil {
+			return err
+		}
+	}
+
 	logger.Info(mainAction, logger.Success, "agency-id", agencyId)
 	return nil
+}
+
+func intToBytes(n uint32) []byte {
+	b := make([]byte, 4)
+	b[0] = byte(n >> 24)
+	b[1] = byte(n >> 16)
+	b[2] = byte(n >> 8)
+	b[3] = byte(n)
+	return b
+}
+
+func bytesToInt(b []byte) uint32 {
+	return (uint32(b[0]) << 24) | 
+	       (uint32(b[1]) << 16) | 
+	       (uint32(b[2]) << 8)  | 
+	       uint32(b[3])
 }
--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,9 +1,7 @@
 import socket
 import logger
 import safe_socket
-
-_ECHO_SERVER_MESSAGE_SIZE = 1024
-
+from src_frozen.lottery import Lottery, Bet
 
 class Server:
     def __init__(self, server_host: str, server_port: int) -> None:
@@ -12,28 +10,75 @@ class Server:
 
     def _handle_client(self, client_socket):
         action = "handle-client"
-        message_amount = 0
+        client_bets = []
+        bets_amount = 0
         try:
             logger.info(action, logger.LogResult.in_progress)
+
+            agency_header = safe_socket.recv_all(client_socket, 4)
+            if not agency_header or len(agency_header) < 4:
+                return
+            agency_id = self._bytes_into_int(agency_header)
+
+            agency_storage_path = f"server_bets_agency_{agency_id}.csv"
+            lottery = Lottery(agency_storage_path)
+            with open(agency_storage_path, "w"):
+                pass
+
             while True:
-                client_message = safe_socket.recv_all(
-                    client_socket, _ECHO_SERVER_MESSAGE_SIZE
+                header = safe_socket.recv_all(client_socket, 4)
+                if not header:
+                    break
+                msg_len = self._bytes_into_int(header)
+                if msg_len == 0:
+                    break
+
+                bet_line_bytes = safe_socket.recv_all(client_socket, msg_len)
+                bet_line = bet_line_bytes.decode('utf-8')
+                fields = bet_line.split(',')
+
+                bet = Bet(
+                    agency_id=agency_id,
+                    first_name=fields[0],
+                    last_name=fields[1],
+                    document=int(fields[2]),
+                    birthdate=fields[3],
+                    number=int(fields[4])
                 )
-                if not client_message:
-                    logger.info(
-                        action,
-                        logger.LogResult.success,
-                        "messages-amount",
-                        message_amount,
-                    )
-                    return
-                message_amount += 1
-                safe_socket.send_all(client_socket, client_message)
+                client_bets.append(bet)
+                bets_amount += 1                
+
+            if client_bets:
+                lottery.store_bets(client_bets)
+            for stored_bet in lottery.load_bets():
+                if lottery.has_won(stored_bet):
+                    winner_line = f"{stored_bet.first_name},{stored_bet.last_name},{stored_bet.document},{stored_bet.birthdate},{stored_bet.number}"
+                    winner_bytes = winner_line.encode('utf-8')
+                    msg_size = len(winner_bytes)
+                    winner_header = self._int_to_bytes(msg_size)
+                    safe_socket.send_all(client_socket, winner_header + winner_bytes)
+
+            end_header = bytes([0, 0, 0, 0])
+            safe_socket.send_all(client_socket, end_header)
+            logger.info(action, logger.LogResult.success, "messages-amount", bets_amount)
+
         except Exception as e:
             logger.error(
-                action, logger.LogResult.fail, "messages-amount", message_amount
+                action, logger.LogResult.fail, "messages-amount", bets_amount
             )
             raise e
+        finally:
+            client_socket.close()
+
+    def _int_to_bytes(self, n: int) -> bytes:
+        b0 = (n >> 24) & 0xFF
+        b1 = (n >> 16) & 0xFF
+        b2 = (n >> 8) & 0xFF
+        b3 = n & 0xFF
+        return bytes([b0, b1, b2, b3])
+
+    def _bytes_into_int(self, b: bytes) -> int:
+        return (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3]
 
     def run(self):
         action = "accept-connection"
