b27307869bf003df548d62a0c849af9e771ec74f
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-08-28 14:41:33 -0300

    Fix: send_all puede mandar 0 bytes en una iteracion sin que sea error

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -4,8 +4,6 @@ def send_all(socket: socket.socket, bytes: bytes):
     sent = 0
     while sent < len(bytes):
         n = socket.send(bytes[sent:])
-        if n == 0:
-            raise RuntimeError("socket connection broken")        
         sent += n
 
 def recv_all(socket: socket.socket, size: int) -> bytes:
