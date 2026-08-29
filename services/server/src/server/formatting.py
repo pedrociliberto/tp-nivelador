999f2d81d878f39e22c444117aebf7fe676d89cd
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-08-29 15:01:18 -0300

    Agrego funciones de parseo y formato en server

---
+++ services/server/src/server/formatting.py
@@ -0,0 +1,15 @@
+from lottery import Lottery, Bet
+
+def parse_bet(self, agency_id: int, bet_line: str) -> Bet:
+        fields = bet_line.split(',')
+        return Bet(
+            agency_id=agency_id,
+            first_name=fields[0],
+            last_name=fields[1],
+            document=int(fields[2]),
+            birthdate=fields[3],
+            number=int(fields[4])
+        )
+
+def format_winner(self, bet: Bet) -> str:
+    return f"{bet.first_name},{bet.last_name},{bet.document},{bet.birthdate},{bet.number}"
