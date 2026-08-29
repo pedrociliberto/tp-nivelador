6447ff060a135830a6cf422395798489ebb522c5
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-08-29 17:02:36 -0300

    Agrego handleo en server por multithreading

--- services/server/src/server/server.py
+++ services/server/src/server/server.py
@@ -1,4 +1,6 @@
 import socket
+import os
+import threading
 import logger
 import protocol
 from . import formatting
@@ -8,6 +10,14 @@ class Server:
     def __init__(self, server_host: str, server_port: int) -> None:
         self.server_host = server_host
         self.server_port = server_port
+        self.quorum_min = int(os.getenv("AGENCY_QUORUM_MIN"))
+        self.quorum_barrier = threading.Barrier(self.quorum_min)
+        self.storage_path = "server_bets.csv"
+        self.lottery = Lottery(self.storage_path)
+        self.lottery_lock = threading.Lock()
+
+        with open(self.storage_path, "w"):
+            pass
 
     def _handle_client(self, client_socket):
         action = "handle-client"
@@ -20,11 +30,6 @@ class Server:
             if agency_id is None:
                 return
 
-            agency_storage_path = f"server_bets_agency_{agency_id}.csv"
-            lottery = Lottery(agency_storage_path)
-            with open(agency_storage_path, "w"):
-                pass
-
             while True:
                 bet_lines = protocol.recv_batch(client_socket)
                 if not bet_lines:
@@ -35,9 +40,15 @@ class Server:
                         bets_amount += 1              
 
             if client_bets:
-                lottery.store_bets(client_bets)
-            for stored_bet in lottery.load_bets():
-                if lottery.has_won(stored_bet):
+                with self.lottery_lock:
+                    self.lottery.store_bets(client_bets)
+
+            logger.info("quorum-wait", logger.LogResult.in_progress, "agency-id", agency_id)
+            self.quorum_barrier.wait()
+            logger.info("quorum-wait", logger.LogResult.success, "agency-id", agency_id)
+
+            for stored_bet in self.lottery.load_bets():
+                if stored_bet.agency_id == agency_id and self.lottery.has_won(stored_bet):
                     winner_line = formatting.format_winner(stored_bet)
                     protocol.send_string_message(client_socket, winner_line)
 
@@ -61,9 +72,13 @@ class Server:
                 try:
                     logger.info(action, logger.LogResult.in_progress)
                     client_socket, _ = server_socket.accept()
+                    logger.info(action, logger.LogResult.success)
+
+                    client_thread = threading.Thread(
+                        target=self._handle_client,
+                        args=(client_socket,)
+                    )
+                    client_thread.start()
                 except Exception as e:
                     logger.error(action, logger.LogResult.fail)
                     raise e
-                logger.info(action, logger.LogResult.success)
-
-                self._handle_client(client_socket)
